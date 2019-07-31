c3fab4d2068e1691fe08b73d349c3a1e56229fc7
yakir golibroda <ygk714@gmail.com>
2019-07-31 19:11:29 +0300

    Add files via upload
    adding even more paramters to the command line.
    should work on 60fps (the output vid should be tested as it is still lacking).
    supports computing the HR in fractions of the forehead and then taking the median result.

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import argparse
 
 # Global variables
 window_size = 300
+fps=30
 lk_params = dict(winSize=(15, 15),
                  maxLevel=2,
                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
@@ -17,10 +18,12 @@ def main():
     parser.add_argument('inputVid',help='Input Video Path', type=str)
     parser.add_argument('outputVid',help='Output Video Path',type=str)
     parser.add_argument('--WinLen',help='Window length',type=int)
+    parser.add_argument('--fps', help='Window length', type=int)
+    parser.add_argument('--xParts', help='Window length', type=int)
+    parser.add_argument('--yParts', help='Window length', type=int)
 
     args=parser.parse_args()
-    if args.WinLen!=None:
-        window_size=args.WinLen
+    vid_name, outputVid, xParts, yParts=handle_arg(args)
 
     # Defining variables
     H_mat = []
@@ -31,6 +34,8 @@ def main():
     p0 = []
     first_p0 = []
     curr_frame = []
+    face_roi=[]
+    face_roi_orig=[]
     curr_gray = []
     forehead_roi = []
     diff_x = 0
@@ -38,7 +43,6 @@ def main():
     mask = np.zeros_like(prv_frame)
 
     # Opening the input video
-    vid_name =args.inputVid
     vidcap = cv2.VideoCapture(vid_name)
     count = 0
     pos_frame = vidcap.get(cv2.CAP_PROP_POS_FRAMES)
@@ -64,11 +68,19 @@ def main():
                 # Find the difference in the roi
                 diff_x, diff_y = get_movement_from_trackers(first_p0, p1)
                 # Get the rgb average in the forehead roi
-                h_avg, s_avg, i_avg = get_forehead_hsv_vectors(curr_frame, forehead_roi, diff_x, diff_y)
-                H_mat.append(h_avg)
-                S_mat.append(s_avg)
-                I_mat.append(i_avg)
-                if (count > window_size) & (count % 30 == 0):
+                H_mat.append(np.zeros(xParts*yParts))
+                S_mat.append(np.zeros(xParts*yParts))
+                I_mat.append(np.zeros(xParts*yParts))
+                for i in range(xParts):
+                    for j in range(yParts):
+                        roi_frac=[forehead_roi[0]+i*(forehead_roi[1]-forehead_roi[0])/3,forehead_roi[1]-(2-i)*(forehead_roi[1]-forehead_roi[0])/3,
+                                  forehead_roi[2] + j * (forehead_roi[3] - forehead_roi[2]) / 2,
+                                  forehead_roi[3] - (1-j) * (forehead_roi[3] - forehead_roi[2]) / 2]
+                        h_avg, s_avg, i_avg = get_forehead_hsv_vectors(curr_frame, roi_frac, diff_x, diff_y)
+                        H_mat[count-1][i+3*j]=h_avg
+                        S_mat[count-1][i + 3 * j]=s_avg
+                        I_mat[count-1][i + 3 * j]=i_avg
+                if (count > window_size) & (count % fps == 0):
                     vec_2_calc = H_mat[count - window_size:count]
                     est_hr_10_sec = get_estimated_heart_rate(vec_2_calc, window_size)
                     hr_vec.append(est_hr_10_sec)
@@ -77,12 +89,12 @@ def main():
             prv_frame = curr_frame
             prv_gray = curr_gray
             count += 1
-            if count%60==0 and count%300!=0:
+            if count%(fps*2)==0 and count%(fps*10)!=0:
                 print ('-'),
-            if count%300==0:
-                print('%d [s]'%(count/30)),
+            if count%(fps*10)==0:
+                print('%d [s]'%(count/fps)),
         else:
-            if (count - 1) % 30 != 0:
+            if (count - 1) % fps != 0:
                 vec_2_calc = H_mat[count - window_size:count]
                 est_hr_10_sec = get_estimated_heart_rate(vec_2_calc, window_size)
                 hr_vec.append(est_hr_10_sec)
@@ -94,7 +106,7 @@ def main():
     vidcap.release()
     hr_vec_print = create_hr_signal(hr_vec, count)
     print('Working on output video')
-    print_hr_to_video(vid_name, args.outputVid, hr_vec, hr_vec_print, face_roi, forehead_roi, face_roi_orig,count)
+    print_hr_to_video(vid_name, outputVid, hr_vec, hr_vec_print, face_roi, forehead_roi, face_roi_orig,count)
     print('Output video done')
 
 
@@ -173,11 +185,18 @@ def get_estimated_heart_rate(color_space, count):
     hf = np.round(count / 15) + 1
     # lf = np.round(count / 40) + 1
     # hf = np.round(count / 10) + 1
-    fft_of_channel = abs(fft(color_space))  # get fft of channel
-    fft_of_channel = fft_of_channel[lf:hf]  # cut out  unnecessary frequencies
-    max_f = np.where(fft_of_channel == np.amax(fft_of_channel))[0][0] + lf  # find max normalized frequency
-    est_heart_rate = max_f * 1800 / count
-
+    c=[]
+    for i in range(len(color_space[0])):
+        c.append(np.zeros(len(color_space)))
+        for j in range(len(color_space)):
+            c[i][j]=color_space[j][i]
+    est_heart_rate=[]
+    for i in range(len(c)):
+        fft_of_channel = abs(fft(c[i]))  # get fft of channel
+        fft_of_channel = fft_of_channel[lf:hf]  # cut out  unnecessary frequencies
+        max_f = np.where(fft_of_channel == np.amax(fft_of_channel))[0][0] + lf  # find max normalized frequency
+        est_heart_rate.append(max_f * (60*fps) / count)
+    est_heart_rate=np.median(est_heart_rate)
     # hr_vec_print = np.zeros(abs(fft(color_space)).shape)
     # hr_vec_print[max_f] = np.amax(fft_of_channel)
     # hr_vec_print = ifft(hr_vec_print)
@@ -239,7 +258,7 @@ def print_hr_to_video(input_vid_name, output_vid_name, hr_print, hr_show, face_r
                 diff_x, diff_y = get_movement_from_trackers(first_p0, p1)
             hr_box_len = min(int(curr_frame.shape[0] * 0.2), int(curr_frame.shape[1] * 0.2))
             curr_frame = cv2.rectangle(curr_frame, (0, 0), (hr_box_len, hr_box_len), (255, 255, 255), cv2.FILLED)
-            curr_frame = cv2.putText(curr_frame, str(hr_print[max((count - window_size) / 30, 0)]),
+            curr_frame = cv2.putText(curr_frame, str(int(hr_print[max((count - window_size) / 30, 0)])),
                                      (int(hr_box_len * 0.05), int(hr_box_len * 0.8)), cv2.FONT_HERSHEY_PLAIN, font_size,
                                      (0, 0, 255), thickness=5)
 
@@ -284,7 +303,7 @@ def get_bodyColor_mask(frame, face_roi):
 
 
 def create_hr_signal(hr_vec, count):
-    hr_vec = np.multiply(hr_vec, window_size) / 1800
+    hr_vec = np.multiply(hr_vec, window_size) / (fps*60)
     phase = np.zeros(count)
     res = np.zeros(count)
     help_idx = 0
@@ -293,10 +312,23 @@ def create_hr_signal(hr_vec, count):
             phase_v = np.float32(hr_vec[help_idx]) / window_size
             phase[i] = phase[i - 1] + phase_v * 2 * np.pi
             res[i] = np.cos(phase[i])
-            if i > window_size and i % 30 == 0:
+            if i > window_size and i % fps == 0:
                 help_idx += 1
     return res
 
+
+def handle_arg(args):
+    if args.WinLen!=None:
+        window_size=args.WinLen
+    if args.fps != None:
+        fps = args.fps
+    x=1
+    y=1
+    if args.xParts!=None:
+        x=args.xParts
+    if args.yParts!=None:
+        y=args.yParts
+    return args.inputVid, args.outputVid, x,y
 
 if __name__ == "__main__":
     main()
